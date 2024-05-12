d399ffc450736134adf691fb67c3f346972a1517
Andrew Chikin <chikin.ap@phystech.edu>
2024-05-12 20:19:58 +0300

    modified remap.lua: added 'w!!'

--- lua/user/vim-config/remap.lua
+++ lua/user/vim-config/remap.lua
@@ -13,10 +13,10 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- This is going to get me cancelled
 -- vim.keymap.set("i", "<C-n>", "<Esc>")
@@ -35,10 +35,10 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 -- unbind
 vim.keymap.set("n", "<C-]>", "<Nop>")
@@ -46,3 +46,5 @@ vim.keymap.set("n", "<C-]>", "<Nop>")
 -- tab switching
 -- vim.keymap.set("n", "<C-Tab>", "<cmd>tabnext<CR>")
 -- vim.keymap.set("n", "<C-S-Tab>", "<cmd>tabprevious<CR>")
+
+vim.keymap.set("c", "w!!", ":w !sudo tee %> /dev/null")
