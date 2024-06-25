d54415d694c2613c9d1695e67265c42328a3a29d
Andrew Chikin <chikin.ap@phystech.edu>
2024-06-26 00:35:38 +0300

    changed keymaps of nvim-tree and neo-tree

--- lua/user/lazy_plugins/nvim-tree.lua
+++ lua/user/lazy_plugins/nvim-tree.lua
@@ -1,7 +1,15 @@
 return {
     "nvim-tree/nvim-tree.lua",
     config = function()
-        vim.keymap.set("n", "<C-t>", "<cmd>NvimTreeToggle<CR>", { desc = "toggle nvim-tree" })
+        vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+        vim.keymap.set(
+            "n",
+            "<leader>ef",
+            "<cmd>NvimTreeFindFileToggle<CR>",
+            { desc = "Toggle file explorer on current file" }
+        )
+        vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+        vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
         require("nvim-tree").setup()
     end,
 }
--- lua/user/plugins-config/telescope-config.lua
+++ lua/user/plugins-config/telescope-config.lua
@@ -1,4 +1,22 @@
-require("telescope").setup({
+local telescope = require("telescope")
+local actions = require("telescope.actions")
+
+telescope.setup({
+    defaults = {
+        path_display = { "smart" },
+        mappings = {
+            i = {
+                ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+                ["<C-j>"] = actions.move_selection_next, -- move to next result
+                ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            },
+        },
+    },
+})
+
+-- telescope.load_extension("fzf")
+
+telescope.setup({
     extensions = {
         ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
