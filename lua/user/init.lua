3d9dd84402ad643f264138eb6af0433a9eac5b46
Andrew Chikin <chikin.ap@phystech.edu>
2024-07-27 11:33:27 +0300

    renamed colors.lua to post_plugins.lua

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,6 +1,5 @@
 require("user.vim-config.options")
 require("user.vim-config.keymaps")
 require("user.lazy_plugins_init")
-require("user.vim-config.colors")
--- require("user.plugins-config.vimtex-config")
+require("user.vim-config.post_plugins")
 require("user.vim-config.autocmd")
