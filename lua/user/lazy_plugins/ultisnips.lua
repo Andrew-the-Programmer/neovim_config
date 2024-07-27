a8cd53d2950766fe7a8ed723cfa2bc3cf938477e
Andrew Chikin <chikin.ap@phystech.edu>
2024-07-27 11:02:54 +0300

    added plugins:     1. ultisnips     2. wal

---
+++ lua/user/lazy_plugins/ultisnips.lua
@@ -0,0 +1,9 @@
+return {
+    "SirVer/ultisnips",
+    config = function()
+        vim.g.UltiSnipsExpandTrigger = "<tab>"
+        vim.g.UltiSnipsJumpForwardTrigger = "<tab>"
+        vim.g.UltiSnipsJumpBackwardTrigger = "<s-tab>"
+    end,
+    opts = {},
+}
